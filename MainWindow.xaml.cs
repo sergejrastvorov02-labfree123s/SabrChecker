9508e9468aa4fc0808a16c93cd0de33980d3b435
sergejrastvorov02-labfree123s <sergejrastvorov02@gmail.com>
2026-05-06 19:29:50 +0300

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,126 +1,224 @@
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Management;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 
 namespace SabrChecker
 {
     public partial class MainWindow : Window
     {
-        private string appPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App");
+        private string method1Text = "vilonity|avalon|kitekat|paste.cc|skyline.fix|masonlite|amphetamine|simplicity|apfetamine|trinity|clarity|NovazBesting|vlone|invis.hack|AMTH|skidware|infinity|AdolfRust|ComputeStringHash|dummy_ptr|facepunch.graphics|norecoil|ExternalCheat_NoRecoil|GxOne|RustExploit_Injector|KaboomCheat|UnderHack|Facepunch.Sharp|BasicLand|GOPOTA|invis|money_rain|superiority|infinity.|astrahookie|geroin|dolbaebfree|novazbesting|CatChair|0xcheat|Dootpeaker.space|skyline.one|lghub|brend|extreme|UnityCrashHandler64|imgui|halal.exe|reg.exe|ak47|berda|Deluxe|Nova|keyran|com.swiftsoft|ANW|UG.dll|cartine.html|plague.dll|plaguecrack.dll|plaguepast.dll|suckmaster|spermaHookie|winhttp.dll|skidware.cc|laze.dll|mortemsuck|AnywareFree|MyCheat.dll|Dast";
+
+        private Dictionary<string, string> tools = new Dictionary<string, string>
+        {
+            { "Everything", "Everything.exe" },
+            { "ShellBagsView", "ShellBags.exe" },
+            { "USBDeview", "USBDeview.exe" },
+            { "LastActivityView", "LastActivityView.exe" },
+            { "BrowserDownloadsView", "BrowserDownloadsView.exe" },
+            { "BrowsingHistoryView", "BrowsingHistoryView.exe" }
+        };
 
         public MainWindow()
         {
             InitializeComponent();
-            Loaded += MainWindow_Loaded;
+
+            this.MouseLeftButtonDown += (s, e) => DragMove();
+
+            LoadWindowIcon();
+            LoadLogo();
+            LoadButtons();
+            LoadSocialIcons();
+            Method1Text.Text = method1Text;
+
+            StartGradientAnimation();
+
+            AppsContent.Visibility = Visibility.Collapsed;
+            InfoContent.Visibility = Visibility.Visible;
+            HighlightTab(InfoTabButton);
         }
 
-        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        private void LoadWindowIcon()
         {
-            await LoadAsync();
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App", "logo.jpg");
+            if (File.Exists(path))
+            {
+                try
+                {
+                    BitmapImage img = new BitmapImage();
+                    img.BeginInit();
+                    img.UriSource = new Uri(path);
+                    img.CacheOption = BitmapCacheOption.OnLoad;
+                    img.EndInit();
+                    this.Icon = img;
+                }
+                catch { }
+            }
         }
 
-        private async System.Threading.Tasks.Task LoadAsync()
+        private void LoadLogo()
         {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App", "logo.jpg");
+            if (File.Exists(path))
+            {
+                try
+                {
+                    BitmapImage img = new BitmapImage();
+                    img.BeginInit();
+                    img.UriSource = new Uri(path);
+                    img.CacheOption = BitmapCacheOption.OnLoad;
+                    img.EndInit();
+                    ServerLogo.Source = img;
+                }
+                catch { }
+            }
+        }
+
+        private void LoadButtons()
+        {
+            MainToolsPanel.Children.Clear();
+            string appFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App");
+
+            foreach (var t in tools)
+            {
+                Button btn = new Button();
+                btn.Content = t.Key;
+                btn.Width = 180;
+                btn.Height = 40;
+                btn.Margin = new Thickness(5);
+                btn.Tag = Path.Combine(appFolder, t.Value);
+                btn.Click += RunProgram;
+                btn.Style = (Style)TryFindResource("ActionButtonStyle");
+                MainToolsPanel.Children.Add(btn);
+            }
+        }
+
+        private void RunProgram(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string path = btn.Tag.ToString();
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("Файл не найден: " + path, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
             try
             {
-                this.Dispatcher.Invoke(() => { LoadingStatus.Text = "Загрузка данных..."; });
-                using (WebClient wc = new WebClient())
-                {
-                    BoxM1.Text = await wc.DownloadStringTaskAsync(new Uri("https://raw.githubusercontent.com/"));
-                }
+                Process.Start(new ProcessStartInfo(path) { Verb = "runas", UseShellExecute = true });
             }
-            catch { BoxM1.Text = "Failed to connect to server."; }
-
-            this.Dispatcher.Invoke(() => { LoadingStatus.Text = "Подготовка..."; });
-            await System.Threading.Tasks.Task.Delay(3000);
-
-            this.Dispatcher.Invoke(() => { LoadingOverlay.Visibility = Visibility.Collapsed; });
-            RefreshSysInfo(null, null);
-        }
-
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-                DragMove();
-        }
-
-        private void SwitchTab(object sender, RoutedEventArgs e)
-        {
-            string tag = (sender as Button).Tag.ToString();
-            TabInfo.Visibility = Visibility.Collapsed;
-            TabApps.Visibility = Visibility.Collapsed;
-            TabSteam.Visibility = Visibility.Collapsed;
-            TabSys.Visibility = Visibility.Collapsed;
-            TabSettings.Visibility = Visibility.Collapsed;
-
-            if (this.FindName(tag) is FrameworkElement tab) tab.Visibility = Visibility.Visible;
-        }
-
-        private void RefreshSysInfo(object sender, RoutedEventArgs e)
-        {
-            try
+            catch (Exception ex)
             {
-                ManagementObjectSearcher mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
-                foreach (ManagementObject mj in mos.Get()) TxtCPU.Text = "CPU: " + mj["Name"];
-
-                mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_VideoController");
-                foreach (ManagementObject mj in mos.Get()) TxtGPU.Text = "GPU: " + mj["Name"];
-
-                mos = new ManagementObjectSearcher("root\\CIMV2", "SELECT TotalPhysicalMemory FROM Win32_ComputerSystem");
-                foreach (ManagementObject mj in mos.Get())
-                {
-                    long ram = Convert.ToInt64(mj["TotalPhysicalMemory"]) / 1024 / 1024 / 1024;
-                    TxtRAM.Text = "RAM: " + ram + " GB";
-                }
-
-                bool obs64 = Process.GetProcessesByName("obs64").Length > 0;
-                bool obs32 = Process.GetProcessesByName("obs32").Length > 0;
-
-                if (obs64 || obs32)
-                {
-                    TxtOBS.Text = "OBS STATUS: DETECTED";
-                    TxtOBS.Foreground = System.Windows.Media.Brushes.Red;
-                }
-                else
-                {
-                    TxtOBS.Text = "OBS STATUS: NOT FOUND";
-                    TxtOBS.Foreground = System.Windows.Media.Brushes.LightGreen;
-                }
+                MessageBox.Show("Ошибка: " + ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-            catch { TxtCPU.Text = "Error reading system info."; }
         }
 
-        private void LaunchApp(object sender, RoutedEventArgs e)
+        private void LoadSocialIcons()
         {
-            string exe = (sender as Button).Tag.ToString();
-            string fullPath = Path.Combine(appPath, exe);
-            if (File.Exists(fullPath))
-                Process.Start(fullPath);
-            else
-                MessageBox.Show("Файл не найден в папке App: " + exe, "Ошибка");
+            LoadIcon(GitHubIcon, "github.ico");
+            LoadIcon(TelegramIcon, "telegram.ico");
+            LoadIcon(DiscordIcon, "discord.ico");
         }
 
-        private void OpenLogsFolder(object sender, RoutedEventArgs e)
+        private void LoadIcon(Image img, string name)
         {
-            if (!Directory.Exists(appPath)) Directory.CreateDirectory(appPath);
-            Process.Start("explorer.exe", appPath);
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App", name);
+            if (File.Exists(path))
+            {
+                try
+                {
+                    BitmapImage bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.UriSource = new Uri(path);
+                    bmp.CacheOption = BitmapCacheOption.OnLoad;
+                    bmp.EndInit();
+                    img.Source = bmp;
+                }
+                catch { }
+            }
         }
 
-        private void OpenGitHub(object sender, RoutedEventArgs e)
+        private void CopyMethod1_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start("https://github.com/");
+            Clipboard.SetText(Method1Text.Text);
+            MessageBox.Show("Скопировано", "Готово", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
-        private void OpenTelegram(object sender, RoutedEventArgs e)
+        private void OpenLink_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start("https://t.me/");
+            Button btn = (Button)sender;
+            string url = "";
+
+            switch (btn.Tag.ToString())
+            {
+                case "github_url": url = "https://github.com/sergejrastvorov02-labfree123s"; break;
+                case "telegram_url": url = "https://t.me/SABRRUST"; break;
+                case "discord_url": url = "https://discord.gg/pcHMTqtEdB"; break;
+            }
+
+            if (!string.IsNullOrEmpty(url))
+                Process.Start(url);
         }
 
-        private void ExitApp(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        private void OpenWebsite_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://sabrrust.gamestores.app/");
+        }
+
+        private void Services_Click(object sender, RoutedEventArgs e) => Process.Start("services.msc");
+        private void DataUsage_Click(object sender, RoutedEventArgs e) => Process.Start("resmon.exe");
+        
+        private void Nvidia_Click(object sender, RoutedEventArgs e)
+        {
+            try { Process.Start("nvcplui.exe"); }
+            catch
+            {
+                try { Process.Start("rundll32.exe", "shell32.dll,Control_RunDLL nvcplui.dll"); }
+                catch { MessageBox.Show("NVIDIA не найдена", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning); }
+            }
+        }
+
+        private void AppsTab_Click(object sender, RoutedEventArgs e)
+        {
+            AppsContent.Visibility = Visibility.Visible;
+            InfoContent.Visibility = Visibility.Collapsed;
+            HighlightTab(AppsTabButton);
+        }
+
+        private void InfoTab_Click(object sender, RoutedEventArgs e)
+        {
+            AppsContent.Visibility = Visibility.Collapsed;
+            InfoContent.Visibility = Visibility.Visible;
+            HighlightTab(InfoTabButton);
+        }
+
+        private void HighlightTab(Button activeTab)
+        {
+            AppsTabButton.Background = Brushes.Transparent;
+            InfoTabButton.Background = Brushes.Transparent;
+            activeTab.Background = new SolidColorBrush(Color.FromRgb(58, 58, 58));
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+        private void Close_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        private void StartGradientAnimation()
+        {
+            var gradient = AnimatedGradient;
+            var stop1 = gradient.GradientStops[0];
+            var stop2 = gradient.GradientStops[1];
+
+            DoubleAnimation anim1 = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(4)) { RepeatBehavior = RepeatBehavior.Forever, AutoReverse = true };
+            DoubleAnimation anim2 = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(4)) { RepeatBehavior = RepeatBehavior.Forever, AutoReverse = true };
+
+            stop1.BeginAnimation(GradientStop.OffsetProperty, anim1);
+            stop2.BeginAnimation(GradientStop.OffsetProperty, anim2);
+        }
     }
 }
